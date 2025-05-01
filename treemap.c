75c6e8e8a268d7341d91c440b458e6318a0cf499
Your Name <you@example.com>
2025-04-30 23:40:39 -0400

    new try

--- treemap.c
+++ treemap.c
@@ -93,7 +93,9 @@ Pair *searchTreeMap(TreeMap *tree, void *key)
         else {
             if (tree->lower_than(key, aux->pair->key) == 1)
                 aux = aux->left;
-
+            
+            else
+                aux = aux->right;
         }
 
     }
